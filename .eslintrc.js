112b6f96152af9ccc4ff3a7962a2b6469c021ef3
Gustav Rasmussen <gcr84@hotmail.com>
2023-08-24 11:32:20 +0200

    test: set project to "tsconfig.json"

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,9 @@ module.exports = {
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module",
-        "project": "",
+        // "project": "",
+        project: "tsconfig.json",
+        // tsconfigRootDir: __dirname,
     },
     "rules": {
     }
