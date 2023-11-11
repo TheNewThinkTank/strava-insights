c18d00ebc9fdba1db6687e6bc96c034b91f62913
Gustav Rasmussen <gcr84@hotmail.com>
2023-11-11 17:26:42 +0100

    chore: add data parser

--- src/convert_data.py
+++ src/convert_data.py
@@ -1,21 +1,50 @@
+"""Take manually downloaded Strava activities data,
+parse them transform them to fit with the data schema
+"""
 
-original_data = "Run Mon, 10/30/2023 Afternoon Run 18:57 4.01 km"
+from datetime import datetime
+import re
 
-parts = original_data.split()
+from icecream import ic
 
-day = parts[1]
-date = parts[2]
-time = parts[5]
+activity_pattern = re.compile(r'\b(\w+)\b')
+date_pattern = re.compile(r'\b(\w{3}, \d{1,2}/\d{1,2}/\d{4})\b')
+time_pattern = re.compile(r'\b(\d{1,2}:\d{2})\b')
+distance_pattern = re.compile(r'\b(\d+(\.\d+)?) km\b')
 
-print(parts)
-# print(date)
+# example = "Run Mon, 10/30/2023 Afternoon Run 18:57 4.01 km"
+example = "Ride	Wed, 9/27/2023	Afternoon Ride	23:00	6.85 km"
 
-month, day, year = date.split('/')
+activity_match = activity_pattern.search(example)
+date_match = date_pattern.search(example)
+time_match = time_pattern.search(example)
+distance_match = distance_pattern.search(example)
 
-activity = parts[0]
+if activity_match:
+    activity_type = activity_match.group(1)
+    ic(activity_type)
+else:
+    print("Activity Type not found")
+if date_match:
+    original_date = date_match.group(1)
+    ic(original_date)
+    activity_date = datetime.strptime(original_date, '%a, %m/%d/%Y').strftime('%b %d, %Y')
+    ic(activity_date)
+else:
+    print("Date not found")
+if time_match:
+    ic(time_match.group(1))
+else:
+    print("Time not found")
+if distance_match:
+    print("Distance:", distance_match.group(1), "km")
+else:
+    print("Distance not found")
 
-formatted_date = f'"{month} {day}, {year}, 00:00:00 PM"'
+# activity_date
+# activity_name
+activity_type
+elapsed_time
+distance
 
-new_data = f'{formatted_date},{activity},{day}'
-
-print(new_data)
+header = "Activity Date,Activity Type,Elapsed Time,Distance"
