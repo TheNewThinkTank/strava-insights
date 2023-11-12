72a3165cf52f16dd26858a9a0e5bc0233104313e
Gustav Rasmussen <gcr84@hotmail.com>
2023-11-12 15:28:47 +0100

    test: parameterize unit tests

--- test/test_convert_data.py
+++ test/test_convert_data.py
@@ -1,26 +1,20 @@
 
+import pytest
+
 from src.convert_data import transform_data
 
 examples = [
-    'Run	Mon, 10/30/2023	Afternoon Run	18:57	4.01 km',
-    'Run	Sun, 10/22/2023	easy jog	23:43	4.50 km',
-    'Ride	Wed, 9/27/2023	Afternoon Ride	23:00	6.85 km',
-    ]
+    'Run\tMon, 10/30/2023\tAfternoon Run\t18:57\t4.01 km',
+    'Run\tSun, 10/22/2023\teasy jog\t23:43\t4.50 km',
+    'Ride\tWed, 9/27/2023\tAfternoon Ride\t23:00\t6.85 km',
+]
 
 
-def test_transform_data():
-    # TODO: parameterize and add all examples
-    example = examples[0]
-
-    activity_date = "Oct 30, 2023, 00:00:00 PM"
-    activity_type = "Run"
-    elapsed_time = "18:57"
-    distance = "4.01"
-
+@pytest.mark.parametrize("example, expected_result", [
+    (examples[0], ("Oct 30, 2023, 00:00:00 PM", "Run", "18:57", "4.01")),
+    (examples[1], ("Oct 22, 2023, 00:00:00 PM", "Run", "23:43", "4.50")),
+    (examples[2], ("Sep 27, 2023, 00:00:00 PM", "Ride", "23:00", "6.85")),
+])
+def test_transform_data(example, expected_result):
     result = transform_data(example)
-
-    assert result[0] == activity_date
-    assert result[1] == activity_type
-    assert result[2] == elapsed_time
-    assert result[3] == distance
-
+    assert result == expected_result
